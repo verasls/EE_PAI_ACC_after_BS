2ea917d17d76398dafc5eba6b3dc01c210ce2f45
Lucas Veras <lucasdsveras@gmail.com>
2019-03-15 13:03:24 +0000

    Initial commit

---
+++ code/functions/get_kcal.R
@@ -0,0 +1,21 @@
+get_kcal <- function(df, ID) {
+  # Computes kilocalory values for a single ID based on VO2 and VCO2
+  #
+  # Args:
+  #   df: a data frame containing data for computation   
+  #   ID: subject ID
+  #
+  # Returns:
+  #   A data frame adding a colunm with computed kilocalory 
+  #   values to the imput data frame
+  
+  require(tidyverse)
+  
+  ID_df <- filter(df, ID == ID)
+  
+  ID_df$kcal <- NA
+  for (i in 1:nrow(ID_df)) {
+    ID_df$kcal[i] <- (3.941 * (ID_df$V_O2[i] / 1000)) + (1.106 * (ID_df$V_CO2[i] / 1000))
+  }
+  return(ID_df)
+}
